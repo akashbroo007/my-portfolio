f871f1418607c2dc29ceb6c5f8d4a636a9d02b88
akashbroo007 <your.email@example.com>
2025-03-30 16:27:43 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '',
   images: {
     domains: [
       'cdn.jsdelivr.net',
+      'cdn.worldvectorlogo.com',
       'upload.wikimedia.org',
       'avatars.githubusercontent.com',
       'images.unsplash.com'
     ],
+    unoptimized: true,
   }
 }
 
